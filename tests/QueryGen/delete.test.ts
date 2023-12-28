2b18996383c600f60f94066d4fff55686a69afcb
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-12-29 03:15:16 +0700

    📦 Added more test case

---
+++ tests/QueryGen/delete.test.ts
@@ -0,0 +1,16 @@
+import { test, expect } from 'vitest';
+import { QueryBuilder } from '../../src';
+
+const tableName = 'flytande';
+
+test(`It should generate raw query DELETE FROM ${tableName}`, function () {
+    const query = QueryBuilder.table(tableName).where('id', 1).delete().toRawQuery();
+
+    expect(query).toBe(`DELETE FROM ${tableName} WHERE id = 1`);
+});
+
+test(`It should generate raw query SELECT id, name, stock, expiration_date FROM ${tableName}`, function () {
+    const query = QueryBuilder.table(tableName).where('expiration_date', '>=', 1).delete().toRawQuery();
+
+    expect(query).toBe(`DELETE FROM ${tableName} WHERE expiration_date >= 1`);
+});
---
+++ tests/QueryGen/select.test.ts
@@ -0,0 +1,16 @@
+import { test, expect } from 'vitest';
+import { QueryBuilder } from '../../src';
+
+const tableName = 'flytande';
+
+test(`It should generate raw query SELECT * FROM ${tableName}`, function () {
+    const query = QueryBuilder.table(tableName).toRawQuery();
+
+    expect(query).toBe(`SELECT * FROM ${tableName}`);
+});
+
+test(`It should generate raw query SELECT id, name, stock, expiration_date FROM ${tableName}`, function () {
+    const query = QueryBuilder.table(tableName).select(['id', 'name', 'stock', 'expiration_date']).toRawQuery();
+
+    expect(query).toBe(`SELECT id, name, stock, expiration_date FROM ${tableName}`);
+});
